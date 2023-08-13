ce321dbe1346006375ec7d9d4b1d2d6ef9489f2d
Pum2A <epros98@gmail.com>
2023-08-13 15:13:37 +0200

    rules added

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -2,7 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rules',
-  templateUrl: './rules.component.html',
+  template: `
+  <div class="rules-main-background-container">
+    <div class="h2-container">
+      <h2>REGULAMIN</h2>
+    </div>
+  <div class="content-container">
+  <ul>
+    <li>Akceptacja Warunków:
+       Użytkownicy muszą zaakceptować regulamin w celu korzystania z serwisu. Korzystając z naszej strony, użytkownik wyraża zgodę na przestrzeganie tych zasad.</li>
+    <li>Odpowiedzialność: Strona nie ponosi odpowiedzialności za straty wynikające z inwestycji w kryptowaluty lub inne aktywa. Inwestowanie wiąże się z ryzykiem, a wszelkie decyzje podejmowane przez użytkowników są ich własnym wyborem.</li>
+    <li>Dane Osobowe: Ze względów bezpieczeństwa i prywatności, strona dba o ochronę danych osobowych użytkowników zgodnie z obowiązującymi przepisami.</li>
+    <li>Zakaz Naruszania Prawa: Użytkownicy zobowiązani są do przestrzegania praw autorskich, patentowych i innych praw własności intelektualnej, a także do niepublikowania treści niezgodnych z prawem.</li>
+  </ul>
+  <ul>
+    <li>Szeroka Różnorodność Treści: Strona udostępnia różnorodne informacje dotyczące kryptowalut, analiz rynkowych i innych związanych tematów, jednak nie gwarantujemy dokładności i kompletności tych treści.Zakaz Nieuprawnionego Dostępu: Zabroni</li>
+    <li>Zakaz Nieuprawnionego Dostępu: Zabronione jest próbowanie uzyskania nieautoryzowanego dostępu do danych innych użytkowników, haseł czy systemów.</li>
+    <li>Zakaz Spamowania: Użytkownicy nie mogą spamować treściami lub komentarzami niezwiązanymi z tematem, ani nie mogą działać w sposób utrudniający korzystanie innym użytkownikom.</li>
+    <li>Zachowanie Kultury: Wszystkie interakcje na stronie muszą być przeprowadzane w sposób kulturalny i szanujący innych użytkowników.</li>
+  </ul>
+  <li>Prawa Autorskie: Wszystkie materiały, treści i znaki towarowe prezentowane na stronie mogą być chronione prawami autorskimi lub innymi prawami własności intelektualnej.</li>
+  <li>Ograniczenia w Użyciu: Użytkownicy nie mają prawa do reprodukowania, publikowania lub wykorzystywania komercyjnego materiałów ze strony bez zgody właściciela.</li>
+  <li>Zastrzeżenie Praw: Strona zastrzega sobie prawo do zmiany treści, regulaminu i zasad korzystania w dowolnym czasie.</li>
+  </div>
+  </div>
+  `,
   styleUrls: ['./rules.component.scss']
 })
 export class RulesComponent {
